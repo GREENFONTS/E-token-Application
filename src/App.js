5bcb3d417a15b02cd773fe0cbeff22bb24c03043
Collins <ezecollins004@gmail.com>
2022-08-16 15:15:58 +0100

    third

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Home from "./Pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoanAppView from "./Pages/LoanAppList";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="Gowns" element={<LoanAppView />}></Route>
+        {/* <Route path="Gowns" element={<LoanAppView />}></Route>
         <Route path="Polo" element={<Borrower />}></Route>
-        <Route path="Tpcs" element={<BorrowerView />}></Route>
-        {/* <Route path="Bags/:itemId" element={<ProductPagebag />}></Route>
-        <Route path="Polo/:itemId" element={<ProductPagepolo />}></Route>
+        <Route path="Tpcs" element={<BorrowerView />}></Route> */}
+        <Route path="/:itemId" element={<LoanAppView />}></Route>
+        {/* <Route path="Polo/:itemId" element={<ProductPagepolo />}></Route>
         <Route path="Gowns/:itemId" element={<ProductPage />}></Route>
         <Route path="Tpcs/:itemId" element={<ProductPage2pcs />}></Route> */}
       </Routes>
